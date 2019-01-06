d2f622cb96aaf8af11c11bdd4fb895bc0e219236
Leonardo Brito <f.leobrito@gmail.com>
2019-01-05 21:51:16 -0300

    Player

---
+++ src/components/Player/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Slider from 'rc-slider';
+import {
+  Container, Current, Progress, Controls, Time, ProgressSlider, Volume,
+} from './styles';
+import VolumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
+
+const Player = () => (
+  <Container>
+    <Current>
+      <img src="https://roquereverso.files.wordpress.com/2017/01/animals.jpg" alt="Pink Floyd" />
+      <div>
+        <span>Pigs</span>
+        <small>Pink Floyd</small>
+      </div>
+    </Current>
+
+    <Progress>
+      <Controls>
+        <button type="button">
+          <img src={ShuffleIcon} alt="Shuffle" />
+        </button>
+        <button type="button">
+          <img src={BackwardIcon} alt="Backward" />
+        </button>
+        <button type="button">
+          <img src={PlayIcon} alt="Play" />
+        </button>
+        <button type="button">
+          <img src={ForwardIcon} alt="Forward" />
+        </button>
+        <button type="button">
+          <img src={RepeatIcon} alt="Repeat" />
+        </button>
+      </Controls>
+      <Time>
+        <span>1:39</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ed760' }}
+            handleStyle={{ border: 0 }}
+            value={50}
+          />
+        </ProgressSlider>
+        <span>3:39</span>
+      </Time>
+    </Progress>
+
+    <Volume>
+      <img src={VolumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: '#404040', borderRadius: 10 }}
+        trackStyle={{ background: '#fff' }}
+        handleStyle={{ display: 'none' }}
+        value={100}
+      />
+    </Volume>
+  </Container>
+);
+
+export default Player;
